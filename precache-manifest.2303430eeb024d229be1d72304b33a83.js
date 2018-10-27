2edb32860346a6d6fe38706de023fc1ea5f10c19
Thomas Point <thomaspoint@gmail.com>
2018-10-27 17:57:54 +0200

    Updates

---
+++ precache-manifest.2303430eeb024d229be1d72304b33a83.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "8621c3fac0e78afe2b71ae3502c30cb5",
+    "url": "/LauraProject/static/media/shopping.8621c3fa.jpg"
+  },
+  {
+    "revision": "ae80fee85d9b23267940084c1b59e327",
+    "url": "/LauraProject/static/media/relooking.ae80fee8.jpg"
+  },
+  {
+    "revision": "aeef18600e4e36bbc5b64348d606dfc6",
+    "url": "/LauraProject/static/media/logo_facebook.aeef1860.png"
+  },
+  {
+    "revision": "6edb371383f270eadb82e53b34723cb2",
+    "url": "/LauraProject/static/media/conseil-tendance.6edb3713.jpg"
+  },
+  {
+    "revision": "11222b1558685266718c",
+    "url": "/LauraProject/static/js/runtime~main.11222b15.js"
+  },
+  {
+    "revision": "01f7acde593e6eb9aa8e",
+    "url": "/LauraProject/static/js/main.01f7acde.chunk.js"
+  },
+  {
+    "revision": "e204711cadb0006f4337",
+    "url": "/LauraProject/static/js/1.e204711c.chunk.js"
+  },
+  {
+    "revision": "01f7acde593e6eb9aa8e",
+    "url": "/LauraProject/static/css/main.870f2fc5.chunk.css"
+  },
+  {
+    "revision": "9942931313b496d7eea526a8d152ef37",
+    "url": "/LauraProject/index.html"
+  }
+];
